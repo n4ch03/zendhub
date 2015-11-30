08f1073c67e13adde00777222ae6bee0db06525e
n4ch03 <iesmite@gmail.com>
2015-11-30 20:07:39 -0300

    Removed useless tests & added push internal comment to zendhub when github issue is closed

--- web-tasks/zendhub-github-webhook-wt.js
+++ web-tasks/zendhub-github-webhook-wt.js
@@ -13,7 +13,7 @@ module.exports = function (context, cb) {
     password: context.data.ZENDESK_PASSWORD,
     domain: context.data.ZENDESK_DOMAIN
   };
-  console.log(zendeskCredentials);
+
 
   if (!isConfiguredProperly()) {
     return cb(409, {"message": "Sorry your webtask does not have " +
@@ -23,7 +23,10 @@ module.exports = function (context, cb) {
   if (context.data.action === 'closed' && re.test(title)) {
     zendeskTicketID = title.match(re)[1];
     var zendeskClient = new ZendeskClient();
-    zendeskClient.updateGithubIssue(zendeskTicketID)
+    zendeskClient.updateTicketTag(zendeskTicketID)
+    .then(function (json) {
+      return zendeskClient.updateTicketComment(zendeskTicketID);
+    })
     .then(function (json) {
       return cb(null, '{"result": "ok"}');
     }).catch(function(err) {
@@ -40,7 +43,7 @@ function ZendeskClient() {
 
 }
 
-ZendeskClient.prototype.updateGithubIssue = function(ticketId) {
+ZendeskClient.prototype.updateTicketTag = function(ticketId) {
   var auth = "Basic " + new Buffer(zendeskCredentials.username + ":"
             + zendeskCredentials.password).toString("base64");
   var options = {
@@ -54,7 +57,31 @@ ZendeskClient.prototype.updateGithubIssue = function(ticketId) {
     body: {
       tags: ["GITHUB-CLOSED"]
     }
-  }
+  };
+  return request(options);
+
+}
+
+ZendeskClient.prototype.updateTicketComment = function(ticketId) {
+  var auth = "Basic " + new Buffer(zendeskCredentials.username + ":"
+            + zendeskCredentials.password).toString("base64");
+  var options = {
+    json: true,
+    method: 'PUT',
+    uri: 'https://'+ zendeskCredentials.domain +'.zendesk.com/api/v2/tickets/'
+          + ticketId + '.json ',
+    headers : {
+      "Authorization" : auth
+    },
+    body: {
+      ticket: {
+        comment: {
+          public: false,
+          body: "This ticket was closed in Github, tag was added."
+        }
+      }
+    }
+  };
   return request(options);
 
 }
--- web-tasks/zendhub-wt-tests.js
+++ web-tasks/zendhub-wt-tests.js
@@ -1,9 +1,11 @@
 var chai = require('chai');
 var bluebird = require('bluebird');
 chai.should();
-var data;
 var mock = require('mock-require');
-var oracle;
+var wtZendesk = require(__dirname + '/zendhub-wt.js');
+var wtGithub = require(__dirname + '/zendhub-github-webhook-wt.js');
+var empty = {"message":"description"}
+
 mock('request-promise', function (options) {
     var url = options.url;
     if (url.indexOf("channels.join") !== -1) {
@@ -23,13 +25,10 @@ mock('request-promise', function (options) {
       options.qs.should.have.property("user", oracle.data.user_id);
       return bluebird.resolve({})
     }
-
-
 });
-var wt = require(__dirname + '/hirebox-wt.js');
 describe('Invite External User Without Channel Creation', function() {
   it('No @', function() {
-    oracle = {
+    /*oracle = {
       data: {
         "SLACK_TOKEN": "A",
         "SLACK_COMMAND_TOKEN": "B",
@@ -46,103 +45,7 @@ describe('Invite External User Without Channel Creation', function() {
     };
     wt(oracle, function (error, body){
       body.should.be.equal("Please provide a valid email");
-    });
-  });
-  it('Valid Email and From Channel', function() {
-    oracle.data.text = "iesmite@gmail.com";
-    oracle.data.test_username = "iesmite";
-    oracle.data.test_email = "iesmite@gmail.com";
-    wt(oracle, function (error, body){
-      body.should.be.equal("User created and invited to channel");
-    });
-  });
-
-  it('Valid Email and From Private Group', function() {
-    oracle.data.channel_name = "privategroup";
-    wt(oracle, function (error, body){
-      body.should.be.equal("You need to be in a channel to invite an external user");
-    });
-  });
-
-  it('Valid Email and From Direct Message', function() {
-    oracle.data.channel_name = "directmessage";
-    wt(oracle, function (error, body){
-      body.should.be.equal("You need to be in a channel to invite an external user");
-    });
-  });
-});
-
-describe('Invite External User and Creates Channel', function() {
-  it('No @', function() {
-    oracle = {
-      data: {
-        "SLACK_TOKEN": "A",
-        "SLACK_COMMAND_TOKEN": "B",
-        "token": "B",
-        "user_id": "USERID",
-        "channel_name": "CHANNEL",
-        "channel_id": "TEST_CHANNEL_ID",
-        "SLACK_CHANNEL_NAME": "PEPITO",
-        "text": "+hihihihi.com",
-        "test_username": "hihihihi.com",
-        "test_email": "hihihihi.com",
-        "SLACK_DOMAIN": 'n4ch03'
-      }
-    };
-    wt(oracle, function (error, body){
-      body.should.be.equal("Please provide a valid email");
-    });
-  });
-  it('Valid Email', function() {
-    oracle.data.text = "+iesmite@gmail.com";
-    oracle.data.test_username = "iesmite";
-    oracle.data.test_email = "iesmite@gmail.com";
-    wt(oracle, function (error, body){
-      body.should.be.equal("Channel And User Created");
-    });  });
-});
-
-describe('Generic Errors', function() {
-  it('No token setted', function() {
-    oracle = {
-      data: {
-        "SLACK_COMMAND_TOKEN": "B",
-        "token": "B",
-        "user_id": "USERID",
-        "channel_name": "CHANNEL",
-        "channel_id": "TEST_CHANNEL_ID",
-        "SLACK_CHANNEL_NAME": "PEPITO",
-        "text": "iesmite@gmail.com",
-        "test_username": "iesmite",
-        "test_email": "iesmite@gmail.com",
-        "SLACK_DOMAIN": 'n4ch03'
-      }
-    };
-    wt(oracle, function (error, body){
-      body.should.be.equal("Sorry the token in you webhook isn't valid");
-    });
-  });
-  it('No API TOKEN setted', function() {
-    oracle.data.SLACK_TOKEN = "A";
-    delete oracle.data.SLACK_COMMAND_TOKEN;
-    wt(oracle, function (error, body){
-      body.should.be.equal("You need to provide the security token when deploy integration webtask");
-    });
-  });
-  it('No POSTFIX setted', function() {
-    oracle.data.SLACK_COMMAND_TOKEN = "B";
-    delete oracle.data.SLACK_CHANNEL_NAME;
-    oracle.data.text = "+iesmite@gmail.com";
-    oracle.data.test_username = "iesmite";
-    oracle.data.test_email = "iesmite@gmail.com";
-    wt(oracle, function (error, body){
-      body.should.be.equal("Channel And User Created");
-    });
-  });
-  it('No SLACK_DOMAIN setted', function() {
-    delete oracle.data.SLACK_DOMAIN;
-    wt(oracle, function (error, body){
-      body.should.be.equal("Your command wasn't configured properly, please provide your slack domain in SLACK_DOMAIN to webtask");
-    });
+    });*/
+    empty.should.have.property("message");
   });
 });
